85f5a48652997c27f5e45e3366976051ed69576b
zdt1985 <zdt1985@yandex.ru>
2016-07-09 17:03:55 +0800

    доработал 4е

--- lesson_1/fourth.java
+++ lesson_1/fourth.java
@@ -7,12 +7,14 @@ package java_1.lesson_1;
 public class fourth {
     public static void main(String[] args){
         System.out.println(test(2, 3));
+        System.out.println(test(5, 5));
         System.out.println(test(7, 8));
+        System.out.println(test(10, 10));
         System.out.println(test(15, 7));
     }
 
     private static boolean test(int a, int b){
-        if (10 < a+b & a+b < 20){
+        if (10 <= a+b & a+b <= 20){
            return true;
         }else{
            return false;
